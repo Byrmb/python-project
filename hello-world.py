06f046c7ffbe925081c6db60794744c6bd92c898
byrm <bayramerbas58@gmail.com>
2022-11-09 13:29:00 +0300

    updated hello world.py

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+git status
+git commit -am "updated hello world.py"
+git status
+git log
+git log --pretty=oneline
+git log --oneline
+git push
+git status
+git commit -am "updated hello world.py"
+git status
+git log
+git log --pretty=oneline
+git log --oneline
+git push
+
